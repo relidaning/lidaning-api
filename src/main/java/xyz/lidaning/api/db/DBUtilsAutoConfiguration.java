413d799442a9b9d932855c14046909ad0f933b60
lidaning <453882101@qq.com>
2022-04-02 10:00:17 +0800

    add autoConfiguration of dbUtils

--- src/main/java/xyz/lidaning/api/db/DBUtilsAutoConfiguration.java
+++ src/main/java/xyz/lidaning/api/db/DBUtilsAutoConfiguration.java
@@ -1,8 +1,14 @@
 package xyz.lidaning.api.db;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
+
+@Configuration
+@ConditionalOnClass({DataSource.class})
 public class DBUtilsAutoConfiguration {
 
     @Bean
